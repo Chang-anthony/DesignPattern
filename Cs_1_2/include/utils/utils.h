cf7148d7d99b8186d639b56feaca4ab0f7a5bb67
TonyChang <a23011257@gmail.com>
2023-08-13 22:55:30 +0800

    Set Vscode env done

--- Cs_1_2/include/utils/utils.h
+++ Cs_1_2/include/utils/utils.h
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <stdio.h>
+#include <string>
+#include <sstream>
+#include <vector>
 
 template<typename T>
 T* requireNonNull(T* object)
@@ -8,9 +11,45 @@ T* requireNonNull(T* object)
         throw std::invalid_argument("object can't be null.");
     return object;
 }
+
 template<typename T>
 void shouldBePositive(T val)
 {
     if(val <= 0)
         throw std::invalid_argument("val must be positive.");
+}
+
+
+template<typename T>
+std::string ToString(const T& val)
+{
+    auto convert = [](const T& val)
+    {
+        std::ostringstream oss;
+        oss << val;
+        return oss.str();
+    };
+
+    return convert(val);
+}
+
+template<typename T>
+std::string ToString(const std::vector<T>& arr)
+{
+    auto convert = [](const T& val)
+    {
+        std::ostringstream oss;
+        oss << val;
+        return oss.str();
+    };
+    
+    std::string result = "{";
+    for(const T& element : arr)
+        result += convert(element) + " , ";
+
+    if(!arr.empty())
+        result = result.substr(0,result.size() - 3);//移除最後兩個空格與逗號
+    result += "]";
+
+    return result;
 }
--- Cs_1_2/main.cpp
+++ Cs_1_2/main.cpp
@@ -1,10 +1,16 @@
+/*
+* @author - a23011257@gmail.com (Anthony)
+*/
 #include <iostream>
 #include <stdio.h>
 #include "include/Student.h"
 
+// TODO:perform addition cal
 
 int main()
 {
     std::cout <<"hello world!!!" << std::endl;
+    int* object = 0;
+    requireNonNull(object);
     return 0;
 }
