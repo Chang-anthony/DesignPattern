48eb9e7fc3714629a925a18f88a238cde6de5502
TonyChang <a23011257@gmail.com>
2023-08-16 17:15:08 +0800

    test

--- Cs_1_2/main.cpp
+++ Cs_1_2/main.cpp
@@ -13,6 +13,7 @@ int main()
     // requireNonNull(object);
     std::string str = "1654135135";
     lengthshouldBe(str,1,5);
+    //lengthshouldBe("123",2,10);
     // Student test = Student("Anthony","123545");
     // test.GainExp(3000);
     return 0;
