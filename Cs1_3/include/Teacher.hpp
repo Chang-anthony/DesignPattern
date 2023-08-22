fc8e2cf6ac3a0d54424f9f9fc8fd74a6051062c3
TonyChang <a23011257@gmail.com>
2023-08-22 20:25:03 +0800

    first commit for Cs13

---
+++ Cs1_3/include/Teacher.hpp
@@ -0,0 +1,23 @@
+#ifndef _TEACHER_H_
+#define _TEACHER_H_
+
+class Teacher
+{
+private:
+    /* data */
+public:
+    Teacher(/* args */);
+    ~Teacher();
+};
+
+Teacher::Teacher(/* args */)
+{
+}
+
+Teacher::~Teacher()
+{
+}
+
+
+
+#endif // __TEACHER_H__
---
+++ Cs1_3/main.cpp
@@ -0,0 +1,15 @@
+/*
+* @author - (Anthony)
+*/
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include "include/Teacher.hpp"
+
+int main()
+{
+    Teacher* teacher = new Teacher();
+    std::cout << "test" << std::endl;
+    return 0;
+}
