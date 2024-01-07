838910a9c4df9ab833b45db48aa87eca3873bfc6
TonyChang <a23011257@gmail.com>
2024-01-07 22:29:00 +0800

    CardPatternHandler Simple done

--- BigTwo/include/bigtwo/CardPatternHandler.hpp
+++ BigTwo/include/bigtwo/CardPatternHandler.hpp
@@ -15,10 +15,13 @@ public:
     CardPatternHandler(CardPatternHandler* next);
     ~CardPatternHandler();
 
-    bool vaild(std::vector<Card*> pattern1,std::vector<Card*> pattern2);
+    bool isSamePatternHandle(std::vector<Card*> pattern1,std::vector<Card*> pattern2);
+    std::string vaildPatternHandle(std::vector<Card*> cards);
+    bool BiggerHandle(std::vector<Card*> pattern1,std::vector<Card*> pattern2);
 
     virtual bool isBigger(std::vector<Card*> pattern1,std::vector<Card*> pattern2) = 0;
-    virtual std::string matchtype(std::vector<Card*> cards) = 0;
+    virtual bool match(std::vector<Card*> cards) = 0;
+    virtual std::string GetPatternName(std::vector<Card*> cards) = 0;
 };
 
 #endif /* _CARDPATTERNHANDLER_H_ */
--- BigTwo/src/bigtwo/CardPatternHandler.cpp
+++ BigTwo/src/bigtwo/CardPatternHandler.cpp
@@ -6,13 +6,35 @@ CardPatternHandler::CardPatternHandler(CardPatternHandler* next):next(next)
 {
 }
 
-bool CardPatternHandler::vaild(std::vector<Card*> pattern1,std::vector<Card*> pattern2)
+bool CardPatternHandler::isSamePatternHandle(std::vector<Card*> pattern1,std::vector<Card*> pattern2)
 {
-    std::string type1 = this->matchtype(pattern1);
-    std::string type2 = this->matchtype(pattern2);
+    std::string type1 = this->vaildPatternHandle(pattern1);
+    std::string type2 = this->vaildPatternHandle(pattern2);
     bool vaild = (type1 != "") && (type2 != "") && (type1 == type2); 
-    
     return vaild;
+}
+
+bool CardPatternHandler::BiggerHandle(std::vector<Card*> pattern1,std::vector<Card*> pattern2)
+{
+    bool IsSame = this->isSamePatternHandle(pattern1,pattern2);
+    if(IsSame)
+    {
+        return this->isBigger(pattern1,pattern2);
+    }
+    else
+        return IsSame;
+}
+
+std::string CardPatternHandler::vaildPatternHandle(std::vector<Card*> cards)
+{
+    if(this->match(cards))
+    {
+        return this->next->GetPatternName(cards);
+    }
+    else
+    {
+        return this->next->vaildPatternHandle(cards);
+    }
 }
 
 CardPatternHandler::~CardPatternHandler()
