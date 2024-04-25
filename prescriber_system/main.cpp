a3bc7ef3727dcc9dbe132490b20c040856236cca
TonyChang <a23011257@gmail.com>
2024-04-25 21:07:38 +0800

    frist commit prescriber system

---
+++ prescriber_system/main.cpp
@@ -0,0 +1,15 @@
+/*
+* @author - (Anthony)
+*/
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+
+
+int main()
+{
+    std::cout << "按下 Enter 鍵已結束遊戲" << std::endl;
+    std::system("pause");
+    return 0;
+}
---
+++ prescriber_system/src/source.cmake
@@ -0,0 +1,12 @@
+set(LIB_NAME "PRESCRIBER_SRC")
+set(LIB_PATH ${CMAKE_CURRENT_LIST_DIR})
+
+set(__SRCS)
+
+# include(${LIB_PATH}/Mapobject/mapobject.cmake)
+# list(APPEND __SRCS ${LIB_SRCS})
+
+# message("${LIB_PATH}")
+
+
+set(LIB_SRCS ${__SRCS})
