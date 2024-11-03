8af6f0d9e97ff453f7ae8833cb71e6bd0139351e
TonyChang <a23011257@gmail.com>
2024-11-03 21:10:53 +0800

    simple done graph hasconnection dfs algorithm but need to test

--- friendship/src/Graph.cpp
+++ friendship/src/Graph.cpp
@@ -20,6 +20,30 @@ Graph::Graph(const std::vector<std::vector<std::string>>& connects)
 //TODO: implement this method
 bool Graph::hasConnection(const std::string& name1, const std::string& name2) 
 {
+    std::set<std::string> visited;
+
+    return dfs(name1, name2, visited);
+}
+
+bool Graph::dfs(const std::string& name1, const std::string& name2, std::set<std::string>& visited) {
+    
+
+    for (int i = 0; i < connections.size(); i++) {
+        if (connections[i][0] == name1) {
+            for (int j = 1; j < connections[i].size(); j++) {
+                if (connections[i][j] == name2) {
+                    return true;
+                }
+                if (visited.find(connections[i][j]) == visited.end()) {
+                    visited.insert(connections[i][j]);
+                    if (dfs(connections[i][j], name2, visited)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+
     return false;
 }
 
