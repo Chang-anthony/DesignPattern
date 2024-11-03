36f5f8f82e82f0fa8d7634a96903de63c71f62c2
TonyChang <a23011257@gmail.com>
2024-11-03 21:34:42 +0800

    simple done graph isMutualFriend function need too test

--- friendship/src/Graph.cpp
+++ friendship/src/Graph.cpp
@@ -5,7 +5,6 @@
 #include <string>
 
 
-
 Graph::Graph()
 {
     connections = std::vector<std::vector<std::string>>();
@@ -17,7 +16,40 @@ Graph::Graph(const std::vector<std::vector<std::string>>& connects)
 }
 
 
-//TODO: implement this method
+//TODO: Need to Test this Function
+std::vector<std::string> Graph::isMutualFriend(std::string name1, std::string name2)
+{
+    std::vector<std::string> mutualFriends;
+
+    std::set<std::string> friends1;
+    for (int i = 0; i < connections.size(); i++) {
+        if (connections[i][0] == name1) {
+            for (int j = 1; j < connections[i].size(); j++) {
+                friends1.insert(connections[i][j]);
+            }
+        }
+    }
+
+    std::set<std::string> friends2;
+    for (int i = 0; i < connections.size(); i++) {
+        if (connections[i][0] == name2) {
+            for (int j = 1; j < connections[i].size(); j++) {
+                friends2.insert(connections[i][j]);
+            }
+        }
+    }
+    
+    for (std::string friend1 : friends1) {
+        if (friends2.find(friend1) != friends2.end()) {
+            mutualFriends.push_back(friend1);
+        }
+    }
+
+    return mutualFriends;
+}
+
+
+//TODO: Need to Test this Function
 bool Graph::hasConnection(const std::string& name1, const std::string& name2) 
 {
     std::set<std::string> visited;
