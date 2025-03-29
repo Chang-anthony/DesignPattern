7903c70a085ccc345644269e1c6e850d26217956
anthony <a23011257@gmail.com>
2025-03-29 15:40:56 +0800

    feat(RPGgame): frist create rpg game codebase

---
+++ RPGGame/main.cpp
@@ -0,0 +1,19 @@
+/*
+* @author - (Anthony)
+*/
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include <fstream>
+
+
+
+int main()
+{
+
+
+    std::cout << "按下 Enter鍵 已結束遊戲" << std::endl;
+    std::system("pause");
+    return 0;
+}
